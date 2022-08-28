0409c66f09e8b288fb0f3ffc711993aca5b32825
nikolaithefirst <87936349+nikolaithefirst@users.noreply.github.com>
2022-08-28 22:43:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import arithmetic_arranger
 
 if __name__ == '__main__':
-    arithmetic_arranger.arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
+    arithmetic_arranger.arithmetic_arranger()
