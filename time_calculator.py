b5483bbd31a2a8ba94bcd5ecd90f8c1c78917268
nikolaithefirst <87936349+nikolaithefirst@users.noreply.github.com>
2022-08-30 10:27:55 +0300

    Update time_calculator.py

--- time_calculator.py
+++ time_calculator.py
@@ -14,6 +14,8 @@ def add_time(begin, additional_time, day=None):
     for key, value in week.items():
         week_reverse[value] = key
 
+    part_of_day = {'AM': 1, 'PM': 0}
+    part_of_day_reverse = {'1': 'AM', '0': 'PM'}
     third = str()
     first_int, second_int, days = int(), int(), int()
     first_int += int(begin[0]) + int(additional_time[0])
@@ -25,14 +27,15 @@ def add_time(begin, additional_time, day=None):
     if second_int < 10:
         second_int = '0' + str(second_int)
     if first_int >= 12:
-
-        if part == 'AM':
-            part = 'PM'
+        print(part)
+        if part_of_day[part] == 1:
+            part = part_of_day_reverse[str((part_of_day[part] + first_int // 12) % 2)]
             days += first_int // 24
+
         else:
-            part = 'AM'
             days += 1
-            days += (first_int) // 24
+            part = part_of_day_reverse[str((part_of_day[part] + first_int // 12) % 2)]
+            days += first_int // 24
 
         first_int = first_int % 12
         if first_int == 0:
@@ -40,15 +43,15 @@ def add_time(begin, additional_time, day=None):
 
     if days:
         if days == 1:
-            third += f'(next day)'
+            third += f' (next day)'
         else:
-            third += f'({days} days later)'
+            third += f' ({days} days later)'
 
     if day:
         day = day.lower()
         day = day[0].upper() + day[1:]
         number = week[day]
-        day = week_reverse[number + days]
-        print(f'{first_int}:{second_int} {part}, {day} {third}')
+        day = week_reverse[(number + days) % 7]
+        return f'{first_int}:{second_int} {part}, {day}{third}'
     else:
-        print(f'{first_int}:{second_int} {part} {third}')
+        return f'{first_int}:{second_int} {part}{third}'
